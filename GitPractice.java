f6353603d8c7ab9337e7bd1fbb5e2aac02cf9174
Alex <adu.alex@yahoo.com>
2017-01-23 19:50:24 +0000

    add

---
+++ GitPractice.java
@@ -0,0 +1,17 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class GitPractice {
+  
+  
+  public static void main(String[] parameters) { 
+    if(parameters[0].equals("add"){
+      double one = Double.parseDouble(parameters[1]);
+      double two = Double.parseDouble(parameters[2]);
+      System.out.println(one + two);
+    }
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
